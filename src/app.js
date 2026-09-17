5133dc1dbb29dfdac640b5dc62cc10f75c2d704f
Abhishek <116945459+Nimmagadda-Abhishek@users.noreply.github.com>
2026-09-17 17:50:56 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.set('trust proxy', 1);
 // Security Middleware
 app.use(helmet());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://globxplorer-design.vercel.app',
   credentials: true,
 }));
 
